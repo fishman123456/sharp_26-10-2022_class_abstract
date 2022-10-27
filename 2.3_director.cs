cdbf8544550270e006f20dd3cc43a6c67cee4557
fishman123456 <den9152@yandex.ru>
2022-10-27 22:51:18 +0700

    4 класса осталось

--- 2.3_director.cs
+++ 2.3_director.cs
@@ -1,6 +1,19 @@
-﻿namespace abstr_Human
+﻿using System.Xml.Linq;
+
+namespace abstr_Human
 {
-    internal class director
+    internal class director : Human
     {
+        string _Add_cab;
+        public director (string Name, string Soneme, int Age, string Add_cab) : base(Name, Soneme, Age)
+        {
+            _Add_cab = Add_cab;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("\tИмя: " + _Name + "\nФамилия: " +
+                _Soname + "\nВозраст: " + _Age + "\n Тип расчета:" +
+                _Add_cab + "\n");
+        }
     }
 }
